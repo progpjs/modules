0ee5d9ade56d2a76b631bf0c2724ecb028652477
johanpiquet <johan.piquet@gmail.com>
2024-02-12 22:23:05 +0100

    Improving nodejs fs support

--- modNodeJs/nodeJsExposedFunctions.go
+++ modNodeJs/nodeJsExposedFunctions.go
@@ -18,90 +18,114 @@ package modNodeJs
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/progpjs/libProgpScripts"
 	"github.com/progpjs/progpAPI"
 	"os"
 	"runtime"
 	"syscall"
+	"time"
 )
 
 func registerExportedFunctions() {
 	rg := libProgpScripts.GetFunctionRegistry()
 	myMod := rg.UseGoNamespace("github.com/progpjs/modules/modNodeJs")
 
+	//region node:process
+
 	modProcess := myMod.UseCustomGroup("nodejsModProcess")
-	modProcess.AddFunction("kill", "JsKill", JsKill)
-	modProcess.AddFunction("cwd", "JsCwd", JsCwd)
-	modProcess.AddFunction("env", "JsEnv", JsEnv)
-	modProcess.AddFunction("arch", "JsArch", JsArch)
-	modProcess.AddFunction("platform", "JsPlatform", JsPlatform)
-	modProcess.AddFunction("argv", "JsArgV", JsArgV)
-	modProcess.AddFunction("exit", "JsExit", JsExit)
-	modProcess.AddFunction("pid", "JsPID", JsPID)
-	modProcess.AddFunction("ppid", "JsPpID", JsPpID)
-	modProcess.AddFunction("chdir", "JsChDir", JsChDir)
-	modProcess.AddFunction("getuid", "JsGetUid", JsGetUid)
-	modProcess.AddAsyncFunction("nextTick", "JsNextTickAsync", JsNextTickAsync)
+	modProcess.AddFunction("kill", "JsProcessKill", JsProcessKill)
+	modProcess.AddFunction("cwd", "JsProcessCwd", JsProcessCwd)
+	modProcess.AddFunction("env", "JsProcessEnv", JsProcessEnv)
+	modProcess.AddFunction("arch", "JsProcessArch", JsProcessArch)
+	modProcess.AddFunction("platform", "JsProcessPlatform", JsProcessPlatform)
+	modProcess.AddFunction("argv", "JsProcessArgV", JsProcessArgV)
+	modProcess.AddFunction("exit", "JsProcessExit", JsProcessExit)
+	modProcess.AddFunction("pid", "JsProcessPID", JsProcessPID)
+	modProcess.AddFunction("ppid", "JsProcessParentPID", JsProcessParentPID)
+	modProcess.AddFunction("chdir", "JsProcessChDir", JsProcessChDir)
+	modProcess.AddFunction("getuid", "JsProcessGetUid", JsProcessGetUid)
+	modProcess.AddAsyncFunction("nextTick", "JsProcessNextTickAsync", JsProcessNextTickAsync)
+
+	//endregion
+
+	//region node:os
 
 	modOS := myMod.UseCustomGroup("nodejsModOS")
-	modOS.AddFunction("homeDir", "JsHomeDir", JsHomeDir)
-	modOS.AddFunction("hostName", "JsHostName", JsHostName)
-	modOS.AddFunction("tempDir", "JsTempDir", JsTempDir)
+	modOS.AddFunction("homeDir", "JsOsHomeDir", JsOsHomeDir)
+	modOS.AddFunction("hostName", "JsOsHostName", JsOsHostName)
+	modOS.AddFunction("tempDir", "JsOsTempDir", JsOsTempDir)
+
+	//endregion
+
+	//region node:fs
+
+	modFS := myMod.UseCustomGroup("nodejsModFS")
+
+	//region Sync
+
+	modFS.AddFunction("existsSync", "JsFsExistsSync", JsFsExistsSync)
+	modFS.AddFunction("statSync", "JsFsStatSync", JsFsStatSync)
+	modFS.AddFunction("accessSync", "JsFsAccessSync", JsFsAccessSync)
+
+	//endregion
+
+	//endregion
 }
 
 //region node:process	(nodejsModProcess)
 
-func JsCwd() string {
+func JsProcessCwd() string {
 	cwd, _ := os.Getwd()
 	return cwd
 }
 
-func JsEnv() progpAPI.StringBuffer {
+func JsProcessEnv() progpAPI.StringBuffer {
 	res := os.Environ()
 	b, _ := json.Marshal(res)
 	return b
 }
 
-func JsArch() string {
+func JsProcessArch() string {
 	// Apple MAC: arm64
 	return runtime.GOARCH
 }
 
-func JsPlatform() string {
+func JsProcessPlatform() string {
 	return runtime.GOOS
 }
 
-func JsArgV() []string {
+func JsProcessArgV() []string {
 	return os.Args
 }
 
-func JsExit(code int) {
+func JsProcessExit(code int) {
 	os.Exit(code)
 }
 
-func JsPID() int {
+func JsProcessPID() int {
 	return os.Getpid()
 }
 
-func JsPpID() int {
+func JsProcessParentPID() int {
 	return os.Getppid()
 }
 
-func JsChDir(dir string) error {
+func JsProcessChDir(dir string) error {
 	return os.Chdir(dir)
 }
 
-func JsGetUid() int {
+func JsProcessGetUid() int {
 	return os.Getuid()
 }
 
-func JsNextTickAsync(fct progpAPI.ScriptFunction) {
+func JsProcessNextTickAsync(fct progpAPI.ScriptFunction) {
 	progpAPI.SafeGoRoutine(func() {
 		fct.CallWithUndefined()
 	})
 }
 
-func JsKill(pid int, signal int) error {
+func JsProcessKill(pid int, signal int) error {
 	err := syscall.Kill(pid, syscall.Signal(signal))
 
 	if signal == 0 {
@@ -119,18 +143,218 @@ func JsKill(pid int, signal int) error {
 
 //region node:os (nodejsModOS)
 
-func JsHomeDir() (string, error) {
+func JsOsHomeDir() (string, error) {
 	dirname, err := os.UserHomeDir()
 	return dirname, err
 }
 
-func JsHostName() (string, error) {
+func JsOsHostName() (string, error) {
 	name, err := os.Hostname()
 	return name, err
 }
 
-func JsTempDir() string {
+func JsOsTempDir() string {
 	return os.TempDir()
 }
+
+//endregion
+
+//region node:fs (nodejsModFS)
+
+//region Structs & Enums
+
+type FsFileState struct {
+	Mode    uint16 `json:"mode"`
+	Size    int64  `json:"size"`
+	Gid     uint32 `json:"gid"`
+	Uid     uint32 `json:"uid"`
+	Dev     int32  `json:"dev"`
+	Rdev    int32  `json:"rdev"`
+	Ino     uint64 `json:"ino"`
+	Nlink   uint16 `json:"nlink"`
+	Blksize int32  `json:"blksize"`
+	Blocks  int64  `json:"blocks"`
+
+	ATimeMs int64  `json:"atimeMs"`
+	Atime   string `json:"atime"`
+
+	MTimeMs int64  `json:"mtimeMs"`
+	Mtime   string `json:"mtime"`
+
+	CTimeMs int64  `json:"ctimeMs"`
+	Ctime   string `json:"ctime"`
+
+	BirthtimeMs int64  `json:"birthtimeMs"`
+	Birthtime   string `json:"birthtime"`
+}
+
+type FsConst uint32
+
+const (
+	FSCONST__UV_FS_SYMLINK_DIR               FsConst = 1
+	FSCONST__UV_FS_SYMLINK_JUNCTION                  = 2
+	FSCONST__O_RDONLY                                = 0
+	FSCONST__O_WRONLY                                = 1
+	FSCONST__O_RDWR                                  = 2
+	FSCONST__UV_DIRENT_UNKNOWN                       = 0
+	FSCONST__UV_DIRENT_FILE                          = 1
+	FSCONST__UV_DIRENT_DIR                           = 2
+	FSCONST__UV_DIRENT_LINK                          = 3
+	FSCONST__UV_DIRENT_FIFO                          = 4
+	FSCONST__UV_DIRENT_SOCKET                        = 5
+	FSCONST__UV_DIRENT_CHAR                          = 6
+	FSCONST__UV_DIRENT_BLOCK                         = 7
+	FSCONST__EXTENSIONLESS_FORMAT_JAVASCRIPT         = 0
+	FSCONST__EXTENSIONLESS_FORMAT_WASM               = 1
+	FSCONST__S_IFMT                                  = 61440
+	FSCONST__S_IFREG                                 = 32768
+	FSCONST__S_IFDIR                                 = 16384
+	FSCONST__S_IFCHR                                 = 8192
+	FSCONST__S_IFBLK                                 = 24576
+	FSCONST__S_IFIFO                                 = 4096
+	FSCONST__S_IFLNK                                 = 40960
+	FSCONST__S_IFSOCK                                = 49152
+	FSCONST__O_CREAT                                 = 512
+	FSCONST__O_EXCL                                  = 2048
+	FSCONST__UV_FS_O_FILEMAP                         = 0
+	FSCONST__O_NOCTTY                                = 131072
+	FSCONST__O_TRUNC                                 = 1024
+	FSCONST__O_APPEND                                = 8
+	FSCONST__O_DIRECTORY                             = 1048576
+	FSCONST__O_NOFOLLOW                              = 256
+	FSCONST__O_SYNC                                  = 128
+	FSCONST__O_DSYNC                                 = 4194304
+	FSCONST__O_SYMLINK                               = 2097152
+	FSCONST__O_NONBLOCK                              = 4
+	FSCONST__S_IRWXU                                 = 448
+	FSCONST__S_IRUSR                                 = 256
+	FSCONST__S_IWUSR                                 = 128
+	FSCONST__S_IXUSR                                 = 64
+	FSCONST__S_IRWXG                                 = 56
+	FSCONST__S_IRGRP                                 = 32
+	FSCONST__S_IWGRP                                 = 16
+	FSCONST__S_IXGRP                                 = 8
+	FSCONST__S_IRWXO                                 = 7
+	FSCONST__S_IROTH                                 = 4
+	FSCONST__S_IWOTH                                 = 2
+	FSCONST__S_IXOTH                                 = 1
+	FSCONST__F_OK                                    = 0
+	FSCONST__R_OK                                    = 4
+	FSCONST__W_OK                                    = 2
+	FSCONST__X_OK                                    = 1
+	FSCONST__UV_FS_COPYFILE_EXCL                     = 1
+	FSCONST__COPYFILE_EXCL                           = 1
+	FSCONST__UV_FS_COPYFILE_FICLONE                  = 2
+	FSCONST__COPYFILE_FICLONE                        = 2
+	FSCONST__UV_FS_COPYFILE_FICLONE_FORCE            = 4
+	FSCONST__COPYFILE_FICLONE_FORCE                  = 4
+)
+
+//endregion
+
+//region Sync
+
+func JsFsExistsSync(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func JsFsStatSync(path string, throwErrorIfMissing bool) (*FsFileState, error) {
+	info, err := os.Stat(path)
+
+	if err != nil {
+		if throwErrorIfMissing {
+			return nil, err
+		}
+
+		return nil, nil
+	}
+
+	osInfo, isUnixFS := info.Sys().(*syscall.Stat_t)
+
+	stat := &FsFileState{}
+
+	stat.Size = info.Size()
+
+	if isUnixFS {
+		stat.Gid = osInfo.Gid
+		stat.Uid = osInfo.Uid
+		stat.Dev = osInfo.Dev
+		stat.Rdev = osInfo.Rdev
+		stat.Ino = osInfo.Ino
+		stat.Mode = osInfo.Mode
+		stat.Nlink = osInfo.Nlink
+		stat.Blksize = osInfo.Blksize
+		stat.Blocks = osInfo.Blocks
+
+		stat.ATimeMs, _ = osInfo.Atimespec.Unix()
+		stat.Atime = time.Unix(osInfo.Atimespec.Sec, osInfo.Atimespec.Nsec).UTC().Format(time.RFC3339Nano)
+
+		stat.MTimeMs, _ = osInfo.Mtimespec.Unix()
+		stat.Mtime = time.Unix(osInfo.Mtimespec.Sec, osInfo.Mtimespec.Nsec).UTC().Format(time.RFC3339Nano)
+
+		stat.CTimeMs, _ = osInfo.Ctimespec.Unix()
+		stat.Ctime = time.Unix(osInfo.Ctimespec.Sec, osInfo.Ctimespec.Nsec).UTC().Format(time.RFC3339Nano)
+
+		stat.BirthtimeMs, _ = osInfo.Birthtimespec.Unix()
+		stat.Birthtime = time.Unix(osInfo.Birthtimespec.Sec, osInfo.Birthtimespec.Nsec).UTC().Format(time.RFC3339Nano)
+	}
+
+	return stat, nil
+}
+
+func JsFsAccessSync(path string, mode int) error {
+	info, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	// https://phoenixnap.com/kb/what-is-umask
+	// User Group Other
+
+	perm := info.Mode().Perm()
+
+	// 0444  =>  4: user can read, 4: group can read, 4: others can read
+	// 0555	 =>  can read & execute
+	// 0666	 =>  can read & write
+	// 0777	 =>  can read & write & execute
+	// 0222	 =>  can write
+	// 0333	 =>  can write & execute
+	// 0111  =>  can execute
+
+	canRead := (perm&0444 == 0444) || (perm&0555 == 0555) || (perm&0666 == 0666) || (perm&0777 == 0777)
+	canWrite := (perm&0222 == 0222) || (perm&0666 == 0666) || (perm&0777 == 0777) || (perm&0333 == 0333)
+	canExecute := (perm&0111 == 0111) || (perm&0555 == 0555) || (perm&0777 == 0777) || (perm&0333 == 0333)
+
+	if mode == FSCONST__F_OK {
+		// F_OK allows testing if file exists.
+		return nil
+	} else {
+		if mode&FSCONST__R_OK == FSCONST__R_OK {
+			if !canRead {
+				return errors.New("can't read")
+			}
+		}
+
+		if mode&FSCONST__W_OK == FSCONST__W_OK {
+			if !canWrite {
+				return errors.New("can't write")
+			}
+		}
+
+		if mode&FSCONST__X_OK == FSCONST__X_OK {
+			if !canExecute {
+				return errors.New("can't execute")
+			}
+		}
+	}
+
+	return nil
+}
+
+//endregion
 
 //endregion
